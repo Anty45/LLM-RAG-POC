e9e6cbf49a60f7849442f4710ff7f2057e039133
anty.koffi <anty.koffi@hokodo.co>
2023-12-02 10:11:14 +0100

    promts

--- src/llm_core/indexing.py
+++ src/llm_core/indexing.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from llama_index import VectorStoreIndex, SummaryIndex
 from llama_index.prompts import PromptTemplate
 
+from prompts import ZERO_SHOT_PROMPT
+
 
 def instantiate_vector_summary_index(documents, service_context):
     vector_index = VectorStoreIndex.from_documents(documents, service_context=service_context)
@@ -16,15 +18,8 @@ def store_index(vector_store: VectorStoreIndex, pth_vector_store: Path):
 
 def get_query_engine(vector_index, service_context, model_config):
     if model_config["text_qa_template"]:
-        text_qa_chat = (
-            "<|system|>: Vous êtes un assistant IA qui répond à la question posée à la fin en utilisant le contexte suivant. Si vous ne connaissez pas la réponse, dites simplement que vous ne savez pas, n'essayez pas d'inventer une réponse. Veuillez répondre exclusivement en français.\n"
-            "<|user|>: {context_str}\n"
-            "Question: {query_str}\n"
-            "<|assistant|>:"
-        )
-        text_qa = PromptTemplate(text_qa_chat)
         query_engine = vector_index.as_query_engine(
-            text_qa_template=text_qa,
+            text_qa_template=ZERO_SHOT_PROMPT,
             service_context=service_context,
             streaming=True
         )
---
+++ src/llm_core/prompts.py
@@ -0,0 +1,19 @@
+from llama_index.prompts import PromptTemplate
+
+ZERO_SHOT_QA_TEMPLATE = (
+    "<|system|>: Reponds à une question à partir du contexte en français.\n"
+    "<|user|>: {query_str} {context_str} \n"
+    "<|Réponse|>:"
+)
+
+ZERO_SHOT_PROMPT = PromptTemplate(ZERO_SHOT_QA_TEMPLATE)
+
+ZERO_SHOT_QUESTION_TEMPLATE = (
+    """
+    "<|system|>: Génère une question brève à partir du contexte suivant en français.\n"
+    "<|user|>: {context_str}\n"
+    "<|Question|>:"
+    """
+)
+
+ZERO_SHOT_QUESTION_PROMPT = PromptTemplate(ZERO_SHOT_QUESTION_TEMPLATE)
